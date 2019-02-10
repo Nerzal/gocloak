8001372615e8cbaf10bc3a1d64c120c563c67a4f
Nerzal <info@noobygames.de>
2019-02-10 21:23:23 +0100

    Extracted anonymous structs

--- pkg/jwx/models.go
+++ pkg/jwx/models.go
@@ -12,30 +12,42 @@ type DecodedAccessTokenHeader struct {
 // Claims served by keycloak inside the accessToken
 type Claims struct {
 	jwt.StandardClaims
-	Typ            string   `json:"typ,omitempty"`
-	Azp            string   `json:"azp,omitempty"`
-	AuthTime       int      `json:"auth_time,omitempty"`
-	SessionState   string   `json:"session_state,omitempty"`
-	Acr            string   `json:"acr,omitempty"`
-	AllowedOrigins []string `json:"allowed-origins,omitempty"`
-	RealmAccess    struct {
-		Roles []string `json:"roles"`
-	} `json:"realm_access"`
-	ResourceAccess struct {
-		RealmManagement struct {
-			Roles []string `json:"roles"`
-		} `json:"realm-management"`
-		Account struct {
-			Roles []string `json:"roles"`
-		} `json:"account"`
-	} `json:"resource_access"`
-	Scope         string `json:"scope"`
-	EmailVerified bool   `json:"email_verified"`
-	Address       struct {
-	} `json:"address"`
-	Name              string `json:"name"`
-	PreferredUsername string `json:"preferred_username"`
-	GivenName         string `json:"given_name"`
-	FamilyName        string `json:"family_name"`
-	Email             string `json:"email"`
+	Typ               string         `json:"typ,omitempty"`
+	Azp               string         `json:"azp,omitempty"`
+	AuthTime          int            `json:"auth_time,omitempty"`
+	SessionState      string         `json:"session_state,omitempty"`
+	Acr               string         `json:"acr,omitempty"`
+	AllowedOrigins    []string       `json:"allowed-origins,omitempty"`
+	RealmAccess       RealmAccess    `json:"realm_access"`
+	ResourceAccess    ResourceAccess `json:"resource_access"`
+	Scope             string         `json:"scope"`
+	EmailVerified     bool           `json:"email_verified"`
+	Address           Address        `json:"address"`
+	Name              string         `json:"name"`
+	PreferredUsername string         `json:"preferred_username"`
+	GivenName         string         `json:"given_name"`
+	FamilyName        string         `json:"family_name"`
+	Email             string         `json:"email"`
+}
+
+// Address TODO what fields does any address have?
+type Address struct {
+}
+
+// RealmAccess holds roles of the user
+type RealmAccess struct {
+	Roles []string `json:"roles"`
+}
+
+type ResourceAccess struct {
+	RealmManagement RealmManagement `json:"realm-management"`
+	Account         Account         `json:"account"`
+}
+
+type RealmManagement struct {
+	Roles []string `json:"roles"`
+}
+
+type Account struct {
+	Roles []string `json:"roles"`
 }
