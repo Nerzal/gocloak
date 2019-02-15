241c2699bd5417fe17e43a730cf36ec683068080
Tobias Theel <tobias.theel@noobygames.de>
2019-02-15 23:18:40 +0100

    Fixed RetrospectToken for KeyCloak versions >= 4.8

--- models.go
+++ models.go
@@ -40,7 +40,7 @@ type RetrospecTokenResult struct {
 	Exp         int               `json:"exp,omitempty"`
 	Nbf         int               `json:"nbf,omitempty"`
 	Iat         int               `json:"iat,omitempty"`
-	Aud         string            `json:"aud,omitempty"`
+	Aud         []string          `json:"aud,omitempty"`
 	Active      bool              `json:"active,omitempty"`
 	AuthTime    int               `json:"auth_time,omitempty"`
 	Jti         string            `json:"jti,omitempty"`
