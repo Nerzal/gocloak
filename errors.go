9473908cca418fbc84d4fcfcfc10922d47cbc4cf
jeEt <jeet.parmar@yahoo.co.in>
2020-03-11 19:46:28 +0530

    the json field coming is not errorMessage
    ```
    ± % http -f POST https://keycloak/auth/realms/{REALM}/protocol/openid-connect/token client_id=account username=asd@asdas.com password=asdsda grant_type=password client_secret=sdasd2-f023-4059-acd9-022ab0e769d9 
    HTTP/1.1 401 Unauthorized
    Cache-Control: no-store
    Connection: keep-alive
    Content-Length: 72
    Content-Type: application/json
    Date: Wed, 11 Mar 2020 14:04:13 GMT
    Pragma: no-cache
    Server: nginx/1.10.3 (Ubuntu)
    
    {
        "error": "invalid_grant",
        "error_description": "Invalid user credentials"
    }
    
    ```

--- errors.go
+++ errors.go
@@ -2,6 +2,6 @@ package gocloak
 
 // HTTPErrorResponse is a model of an error response
 type HTTPErrorResponse struct {
-	ErrorMessage string `json:"errorMessage,omitempty"`
+	ErrorMessage string `json:"error_description,omitempty"`
 	Error        string `json:"error,omitempty"`
 }
