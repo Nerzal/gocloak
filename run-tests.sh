03434ef975a6abfb3f793669d3f73a137be09a15
viniciusousazup <vinicius.sousa@zup.com.br>
2020-06-18 20:08:49 -0300

    change mkdir

--- run-tests.sh
+++ run-tests.sh
@@ -5,7 +5,7 @@ docker-compose up -d
 
 sleep 10
 
-mkdir bin
+mkdir -p bin
 go test -failfast -race -cover -coverprofile=bin/coverage.txt -covermode=atomic -cpu 1,2 -bench . -benchmem
 
 docker-compose down
