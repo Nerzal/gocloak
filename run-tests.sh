0a88c7430ae611c6a493d7b19351839dfce4dfc0
viniciusousazup <vinicius.sousa@zup.com.br>
2020-06-18 18:32:32 -0300

    fix docker-compose

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+docker-compose down
 docker-compose up -d
 
-go test -failfast -race -cover -coverprofile=coverage.txt -covermode=atomic -cpu 1,2 -bench . -benchmem
+sleep 10
+
+mkdir bin
+go test -failfast -race -cover -coverprofile=bin/coverage.txt -covermode=atomic -cpu 1,2 -bench . -benchmem
 
 docker-compose down
