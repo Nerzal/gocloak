6ef46d08353cf4b606e327f761667796d61524f1
viniciusousazup <vinicius.sousa@zup.com.br>
2020-06-19 15:34:24 -0300

    change sleep to 30 on pipeline

--- run-tests.sh
+++ run-tests.sh
@@ -3,7 +3,7 @@
 docker-compose down
 docker-compose up -d
 
-sleep 10
+sleep 30
 
 mkdir -p bin
 go test -failfast -race -cover -coverprofile=bin/coverage.txt -covermode=atomic -cpu 1,2 -bench . -benchmem
