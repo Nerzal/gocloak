19ad8789e8738c1029d87e9e00db42ea780dcafc
akoserwa <akoserwa@redhat.com>
2021-04-08 21:59:38 +0530

    updating spell

--- models.go
+++ models.go
@@ -602,11 +602,11 @@ type ProtocolMapperRepresentation struct {
 
 // GetClientsParams represents the query parameters
 type GetClientsParams struct {
-	ClientID            *string `json:"clientId,omitempty"`
-	ViewableOnly        *bool   `json:"viewableOnly,string,omitempty"`
-	First               *int    `json:"first,string,omitempty"`
-	Max                 *int    `json:"max,string,omitempty"`
-	SearchableAttribute *string `json:"q,string,omitempty"`
+	ClientID             *string `json:"clientId,omitempty"`
+	ViewableOnly         *bool   `json:"viewableOnly,string,omitempty"`
+	First                *int    `json:"first,string,omitempty"`
+	Max                  *int    `json:"max,string,omitempty"`
+	SearchableAttributes *string `json:"q,string,omitempty"`
 }
 
 // UserInfoAddress is representation of the address sub-filed of UserInfo
